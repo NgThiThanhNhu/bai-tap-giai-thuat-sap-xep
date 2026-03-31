78962a2a49116a13f7a2a6ef80eb7dc049781d97
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2026-03-31 11:13:29 +0700

    them bai 1

---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Nhap so phan tu cua mang: ");
+int n = int.Parse(Console.ReadLine());
+int[] arr = new int[n];
+for (int i=0; i<n; i++)
+{
+    Console.Write("Phan tu - [{0}]: ", i);
+    arr[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Mang vua nhap");
+for (int i=0; i<n; i++)
+{
+    Console.Write($"{arr[i]} ");
+}
+Console.WriteLine("\n---------------BUBBLE SORT---------------\n");
+
+for (int i=0; i<n; i++)
+{
+    for (int j=i+1; j<n; j++)
+    {
+        if (arr[i] > arr[j])
+        {
+            int temp;
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
+
+for (int i=0; i<n; i++)
+{
+    Console.Write($"{arr[i]} ");
+}
